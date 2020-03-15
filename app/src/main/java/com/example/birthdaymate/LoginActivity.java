1212266823c5c19a0e24478274fa9bfd291f2642
sodimu segun <princesegzy01@yahoo.com>
2020-03-15 10:07:49 +0100

    add login Activity

---
+++ app/src/main/java/com/example/birthdaymate/LoginActivity.java
@@ -0,0 +1,42 @@
+package com.example.birthdaymate;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+        setContentView(R.layout.activity_login);
+//        setContentView(R.layout.activity_main);
+    }
+
+    public void Login (View view){
+        EditText username = findViewById(R.id.email);
+        EditText password = findViewById(R.id.password);
+
+        String usernameString = username.getText().toString();
+        String passwordString = password.getText().toString();
+
+        if(usernameString.isEmpty()){
+            Toast t = Toast.makeText(this, "Email field cannot be empty", 3);
+            t.show();
+            return;
+        }
+
+        if(passwordString.isEmpty()){
+            Toast t = Toast.makeText(this, "Password field cannot be empty", 3);
+            t.show();
+            return;
+        }
+    }
+}
--- app/src/main/java/com/example/birthdaymate/MainActivity.java
+++ app/src/main/java/com/example/birthdaymate/MainActivity.java
@@ -4,6 +4,11 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.view.View;
+
+import android.content.Intent;
+
+
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,10 +16,16 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        
+//        setContentView(R.layout.activity_main);
+
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void SendMessage(View view){
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 }
