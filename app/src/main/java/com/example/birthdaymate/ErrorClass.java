a0e019d7541e69952e2bdd20a89d07f98b1b839b
sodimu segun <princesegzy01@yahoo.com>
2020-03-16 09:05:30 +0100

    added progressbar to register activity

---
+++ app/src/main/java/com/example/birthdaymate/ErrorClass.java
@@ -0,0 +1,12 @@
+package com.example.birthdaymate;
+
+public class ErrorClass {
+
+    public String status;
+    public String msg;
+
+    public ErrorClass(String status, String msg){
+        this.status = status;
+        this.msg = msg;
+    }
+}
--- app/src/main/java/com/example/birthdaymate/RegisterActivity.java
+++ app/src/main/java/com/example/birthdaymate/RegisterActivity.java
@@ -4,22 +4,48 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Build;
+import android.util.Log;
 import android.view.View;
 
 import android.os.Bundle;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.androidnetworking.AndroidNetworking;
+import com.androidnetworking.common.Priority;
+import com.androidnetworking.error.ANError;
+import com.androidnetworking.interfaces.JSONArrayRequestListener;
+import com.androidnetworking.interfaces.JSONObjectRequestListener;
+import com.google.gson.Gson;
+
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
 public class RegisterActivity extends AppCompatActivity {
+
+    ProgressBar simpleProgressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        AndroidNetworking.initialize(getApplicationContext());
+
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
         setContentView(R.layout.activity_register);
+
+        simpleProgressBar = findViewById(R.id.progressBar);
+        simpleProgressBar.setVisibility(View.INVISIBLE);
     }
 
     public void gotoLogin(View view){
@@ -27,7 +53,10 @@ public class RegisterActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+//    @android.support.annotation.RequiresApi(api = Build.VERSION_CODES.GINGERBREAD)
     public void  Register(View view){
+
+
 
         EditText nameText = findViewById(R.id.name);
         EditText dobText = findViewById(R.id.dob);
@@ -36,7 +65,7 @@ public class RegisterActivity extends AppCompatActivity {
 
         if( nameText.getText().toString().isEmpty()){
             @SuppressLint("WrongConstant")
-            Toast t = Toast.makeText(this, "Name field cannot be empty", 3);
+            Toast t = Toast.makeText(this, "userName field cannot be empty", 3);
             t.show();
             return;
         }
@@ -61,6 +90,78 @@ public class RegisterActivity extends AppCompatActivity {
             t.show();
             return;
         }
+
+        JSONObject jsonObject = new JSONObject();
+        try {
+            jsonObject.put("userName", nameText.getText().toString().trim());
+            jsonObject.put("email", emailText.getText().toString().trim());
+            jsonObject.put("dob", dobText.getText().toString().trim());
+            jsonObject.put("password", passwordText.getText().toString().trim());
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        simpleProgressBar.setVisibility(View.VISIBLE);
+        AndroidNetworking.post("http://192.168.43.103:9000/api/account/register")
+        .addJSONObjectBody(jsonObject) // posting json
+        .setTag("test")
+        .setPriority(Priority.IMMEDIATE)
+        .build()
+        .getAsJSONObject(new JSONObjectRequestListener() {
+            @Override
+            public void onResponse(JSONObject response) {
+                // do anything with response
+                simpleProgressBar.setVisibility(View.INVISIBLE);
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(RegisterActivity.this);
+                alertDialogBuilder.setMessage("Account registration was successful");
+                alertDialogBuilder.setNeutralButton("Ok",
+                        new DialogInterface.OnClickListener() {
+                            @SuppressLint("WrongConstant")
+                            @Override
+                            public void onClick(DialogInterface arg0, int arg1) {
+                                Toast.makeText(RegisterActivity.this,"Registration successful",3).show();
+
+                                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                                startActivity(intent);
+                            }
+                        });
+
+                AlertDialog alertDialog = alertDialogBuilder.create();
+                alertDialog.show();
+            }
+
+            @Override
+            public void onError(ANError anError) {
+
+                Gson g = new Gson();
+                final ErrorClass err = g.fromJson(anError.getErrorBody().toString(), ErrorClass.class);
+                Log.e("errorRest", err.msg);
+
+                simpleProgressBar.setVisibility(View.INVISIBLE);
+
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(RegisterActivity.this);
+                alertDialogBuilder.setMessage(err.msg);
+                alertDialogBuilder.setNegativeButton("Ok",
+                        new DialogInterface.OnClickListener() {
+                            @SuppressLint("WrongConstant")
+                            @Override
+                            public void onClick(DialogInterface arg0, int arg1) {
+                                Toast.makeText(RegisterActivity.this,err.msg,3).show();
+                                return;
+                            }
+                        });
+
+                AlertDialog alertDialog = alertDialogBuilder.create();
+                alertDialog.show();
+
+
+
+
+            }
+        });
+
+
 
     }
 }
