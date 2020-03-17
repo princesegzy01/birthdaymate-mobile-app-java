7f4d77873d20b5620c576177805c2e3a7c564dc6
sodimu segun <princesegzy01@yahoo.com>
2020-03-17 03:07:05 +0100

    add splash screen

---
+++ app/src/main/java/com/example/birthdaymate/SplashActivity.java
@@ -0,0 +1,27 @@
+package com.example.birthdaymate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
+
+public class SplashActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        //Splash Screen duration
+        int secondsDelayed = 1;
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        }, secondsDelayed * 4000);
+    }
+}
